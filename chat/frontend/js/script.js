7fccb7e12ccdd1ba114d61a73056a39f5663a028
raffsdias <140096210+raffsdias@users.noreply.github.com>
2023-11-10 16:48:18 -0300

    Update script.js
    Updated the url of connection

--- chat/frontend/js/script.js
+++ chat/frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = () => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-93ol.onrender.com")
     websocket.onmessage = processMessage
 
 
